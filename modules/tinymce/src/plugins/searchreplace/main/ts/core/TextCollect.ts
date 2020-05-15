9702f616392a769bb4b3d4fdd0b7d27716feff99
Lee Newson <lee.newson@tiny.cloud>
2020-05-15 12:43:13 +1000

    TINY-5999: Fixed issues with collecting text nodes from nested contenteditable elements

--- modules/tinymce/src/plugins/searchreplace/main/ts/core/TextCollect.ts
+++ modules/tinymce/src/plugins/searchreplace/main/ts/core/TextCollect.ts
@@ -7,17 +7,22 @@
 
 import { Node, Range, Text } from '@ephox/dom-globals';
 import { Arr, Fun, Obj } from '@ephox/katamari';
+import { Node as SandNode } from '@ephox/sand';
 import { Element, SelectorFilter, Traverse } from '@ephox/sugar';
 import DOMUtils from 'tinymce/core/api/dom/DOMUtils';
 import TreeWalker from 'tinymce/core/api/dom/TreeWalker';
 import { TextSection } from './Types';
 
-type CollectContentEditableFn = (node: Node) => TextSection[];
-type CollectTextFn = (node: Text, section: TextSection) => void;
 interface WalkerCallbacks {
   boundary: (node: Node) => boolean;
   cef: (node: Node) => boolean;
   text: (node: Text) => void;
+}
+
+interface CollectSettings {
+  cef?: (node: Node) => TextSection[];
+  text?: (node: Text, section: TextSection) => void;
+  skipStart: boolean;
 }
 
 const isBoundary = (dom: DOMUtils, node: Node) => dom.isBlock(node) || Obj.has(dom.schema.getShortEndedElements(), node.nodeName);
@@ -64,7 +69,7 @@ const walk = (dom: DOMUtils, walkerFn: (shallow?: boolean) => Node, startNode: N
 
 const collectTextToBoundary = (dom: DOMUtils, section: TextSection, node: Node, rootNode: Node, forwards: boolean) => {
   // Don't bother collecting text nodes if we're already at a boundary
-  if (isBoundary(dom, node)) {
+  if (isBoundary(dom, node) || isHidden(dom, node) || isContentEditableFalse(dom, node) || dom.getContentEditable(node) === 'true') {
     return;
   }
 
@@ -88,7 +93,7 @@ const collectTextToBoundary = (dom: DOMUtils, section: TextSection, node: Node, 
   });
 };
 
-const collect = (dom: DOMUtils, rootNode: Node, startNode: Node, endNode?: Node, text?: CollectTextFn, cef?: CollectContentEditableFn, skipStart: boolean = true) => {
+const collect = (dom: DOMUtils, rootNode: Node, startNode: Node, endNode: Node, settings: CollectSettings) => {
   const walker = new TreeWalker(startNode, rootNode);
   const sections: TextSection[] = [];
   let current: TextSection = nuSection();
@@ -111,18 +116,18 @@ const collect = (dom: DOMUtils, rootNode: Node, startNode: Node, endNode?: Node,
     cef: (node) => {
       finishSection();
       // Collect additional nested contenteditable true content
-      if (cef) {
-        sections.push(...cef(node));
+      if (settings.cef) {
+        sections.push(...settings.cef(node));
       }
       return false;
     },
     text: (next) => {
       current.elements.push(Element.fromDom(next));
-      if (text) {
-        text(next, current);
+      if (settings.text) {
+        settings.text(next, current);
       }
     }
-  }, endNode, skipStart);
+  }, endNode, settings.skipStart);
 
   // Find any text between the end node and the closest boundary, then finalise the section
   if (endNode) {
@@ -139,17 +144,26 @@ const collectRangeSections = (dom: DOMUtils, rng: Range): TextSection[] => {
   const end = toLeaf(rng.endContainer, rng.endOffset);
   const endNode = end.element().dom();
 
-  return collect(dom, rng.commonAncestorContainer, startNode, endNode, (node, section) => {
-    // Set the start/end offset of the section
-    if (node === endNode) {
-      section.fOffset += node.length - end.offset();
-    } else if (node === startNode) {
-      section.sOffset += start.offset();
-    }
-  }, (node) => Arr.bind(SelectorFilter.descendants(Element.fromDom(node), '*[contenteditable=true]'), (e) => {
-    const ceTrueNode = e.dom();
-    return collect(dom, ceTrueNode, ceTrueNode);
-  }), false);
+  return collect(dom, rng.commonAncestorContainer, startNode, endNode, {
+    text: (node, section) => {
+      // Set the start/end offset of the section
+      if (node === endNode) {
+        section.fOffset += node.length - end.offset();
+      } else if (node === startNode) {
+        section.sOffset += start.offset();
+      }
+    },
+    cef: (node) => {
+      // Collect the sections and then order them appropriately, as nested sections maybe out of order
+      // TODO: See if we can improve this to avoid the sort overhead
+      const sections = Arr.bind(SelectorFilter.descendants(Element.fromDom(node), '*[contenteditable=true]'), (e) => {
+        const ceTrueNode = e.dom();
+        return collect(dom, ceTrueNode, ceTrueNode, undefined, { skipStart: true });
+      });
+      return Arr.sort(sections, (a, b) => (SandNode.documentPositionPreceding(a.elements[0].dom(), b.elements[0].dom())) ? 1 : -1);
+    },
+    skipStart: false
+  });
 };
 
 const fromRng = (dom: DOMUtils, rng: Range): TextSection[] => rng.collapsed ? [] : collectRangeSections(dom, rng);
